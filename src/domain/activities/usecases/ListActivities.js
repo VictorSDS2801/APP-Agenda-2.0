7da203bb90da836242d2c9e64a2938ff1a1be80b
Victor Diogo <victordiogo2801@gmail.com>
2026-02-11 21:07:07 -0300

    ajustando erros

--- src/domain/activities/usecases/ListActivities.js
+++ src/domain/activities/usecases/ListActivities.js
@@ -1,47 +1,62 @@
 class ListActivities {
-    constructor(activityRepository) {
-        this.activityRepository = activityRepository
+  constructor(activityRepository) {
+    this.activityRepository = activityRepository;
+  }
+
+  async execute(filters = {}) {
+    let queryFilters = {};
+
+    if (filters.tomorrow) {
+      // Calcular data de amanhã em UTC
+      const now = new Date();
+      const tomorrow = new Date(Date.UTC(
+        now.getUTCFullYear(),
+        now.getUTCMonth(),
+        now.getUTCDate() + 1,
+        0, 0, 0, 0
+      ));
+      
+      const dayAfter = new Date(Date.UTC(
+        now.getUTCFullYear(),
+        now.getUTCMonth(),
+        now.getUTCDate() + 2,
+        0, 0, 0, 0
+      ));
+
+      console.log('🔍 Filtro Tomorrow - De:', tomorrow, 'Até:', dayAfter);
+
+      queryFilters.dueDate = {
+        $gte: tomorrow,
+        $lt: dayAfter
+      };
     }
 
-    async execute(filters = {}) {
-        let queryFilters = {}
-
-        if (filters.tomorrow) {
-            const tomorrow = new Date()
-            tomorrow.setUTCDate(tomorrow.getUTCDate() + 1)
-            tomorrow.setUTCHours(0, 0, 0, 0)
-
-            const dayAfter = new Date(tomorrow)
-            dayAfter.setUTCDate(dayAfter.getUTCDate() + 1)
-
-            queryFilters.dueDate = {
-                $gte: tomorrow,
-                $lt: dayAfter
-            }
-        }
-
-        if (filters.startDate) {
-            queryFilters.issueDate = {
-                ...queryFilters.issueDate,
-                $gte: new Date(filters.startDate)
-            }
-        }
-
-        if (filters.endDate) {
-            queryFilters.dueDate = {
-                ...queryFilters.dueDate,
-                $lte: new Date(filters.endDate)
-            }
-        }
-
-        if (filters.subject) {
-            queryFilters.subject = filters.subject
-        }
-
-        const activities = await this.activityRepository.findAll(queryFilters)
-
-        return activities
+    if (filters.startDate) {
+      queryFilters.issueDate = {
+        ...queryFilters.issueDate,
+        $gte: new Date(filters.startDate + 'T00:00:00.000Z')
+      };
     }
+
+    if (filters.endDate) {
+      queryFilters.dueDate = {
+        ...queryFilters.dueDate,
+        $lte: new Date(filters.endDate + 'T23:59:59.999Z')
+      };
+    }
+
+    if (filters.subject) {
+      queryFilters.subject = filters.subject;
+    }
+
+    console.log('🔍 QueryFilters final:', queryFilters);
+
+    const activities = await this.activityRepository.findAll(queryFilters);
+
+    console.log('🔍 Atividades encontradas:', activities.length);
+
+    return activities;
+  }
 }
 
-module.exports = ListActivities
+module.exports = ListActivities;
